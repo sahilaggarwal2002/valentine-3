66d690ae9a82113d4affb4d9bbc88471c336dd4b
sahilaggarwal2002 <44200191+sahilaggarwal2002@users.noreply.github.com>
2026-02-03 19:54:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
     title.textContent = "Yippeeee!";
 
-    catImg.src = "cat_dance.gif";
+    catImg.src = "cat_dance.jpeg";
 
     document.querySelector(".letter-window").classList.add("final");
 
